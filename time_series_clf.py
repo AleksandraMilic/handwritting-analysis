ac4788086de6e435c0eb4c85c95e8a05101c74fa
AleksandraMilic <aleksandra.milic02@gmail.com>
2021-08-14 22:24:58 +0200

    distributed training set??

--- time_series_clf.py
+++ time_series_clf.py
@@ -1,8 +1,15 @@
 from recurent_nn import *
+from tensorflow import keras
+# from tensorflow.keras import layers
+import time
+import numpy as np
+import pandas as pd
+from pandas.core.frame import DataFrame
 
 def find_min_t(X_test):
     min_t = 10**6
     for handwritting in X_test:
+        print(len(handwritting))
         if len(handwritting) < min_t:
             min_t = len(handwritting)
 
@@ -24,24 +31,29 @@ def export_results2(score,acc):
 def real_time(model,X_train, Y_train, X_test, Y_test):
     score_list = []
     accuracy_list = []
-    train_index_list = []
-    test_index_list = []
-    parameters = []
     epochs = 1
     
-    print('real_time')
+    print('----------real_time------------')
     min_t = find_min_t(X_test)
     step =  min_t // 3
+    print('min_t',min_t)
 
     for i in range(1,min_t,step):
-        new_test_X = [x[:i] for x in X_train]
-        new_test_Y = Y_test[:i] 
+
+        new_test_X = X_test[:]
+        for x in range(len(X_test)):
+            new_test_X[x] = new_test_X[x][:i]  ###IGNORE 
+        X_test1 = tf.ragged.constant(new_test_X)
+        X_test1 = X_test1.to_tensor()
+
+
+        print(X_test1.shape)
 
         model.fit(X_train, Y_train, 
                     epochs=epochs, batch_size=1,
-                    validation_data=(new_test_X, new_test_Y))
+                    validation_data=(X_test1, Y_test))
 
-        score,acc = model.evaluate(new_test_X, new_test_Y, verbose = 2)
+        score,acc = model.evaluate(X_test1, Y_test, verbose = 2)
         print("Logloss score: %.2f" % (score))
         print("Validation set Accuracy: %.2f" % (acc))
 
