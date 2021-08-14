e6ef5df0944ea79c907815399cbf44d49059f4be
AleksandraMilic <aleksandra.milic02@gmail.com>
2021-08-14 19:37:50 +0200

    changed parameters

--- get_model.py
+++ get_model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def get_model(X_train):
+def get_model(X_train,lr,activation):
     model = keras.Sequential()
 
     # model.add(layers.Embedding(input_dim=1000, output_dim=4))
@@ -13,26 +13,17 @@ def get_model(X_train):
     model.add(layers.Input(shape=[None, max_seq], dtype=tf.float32, ragged=True))  #Warning: use InputLayer?
     #, batch_size=99
     
-    model.add(layers.LSTM(128))
-    model.add(layers.Dropout(0.2))
+    model.add(layers.LSTM(200)) #128
+    model.add(layers.Dropout(0.01)) #0.2
 
-    # model.add(layers.LSTM(128, activation='relu'))
+
+    model.add(layers.Dense(64, activation=activation)) #32,relu
+    model.add(layers.Dropout(0.01)) #0.2
+
+    model.add(layers.Dense(4, activation='softmax')) #class - 4
     # model.add(layers.Dropout(0.2))
 
-    # model.add(layers.Dense(32, activation='relu'))
-    # model.add(layers.Dropout(0.2))
-
-    model.add(layers.Dense(4, activation='softmax')) #class
-    # model.add(layers.Dropout(0.2))
-
-    
-
-    # model.compile(
-    # loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    # optimizer="sgd",
-    # metrics=["accuracy"],)
-
-    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+    opt = tf.keras.optimizers.Adam(lr=lr) #0.001, decay=1e-5
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
     model.summary()
 
