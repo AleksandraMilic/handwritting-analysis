ec4550e7f372f623156ef0d9977f5cb2d217100f
AleksandraMilic <aleksandra.milic02@gmail.com>
2021-09-03 01:43:18 +0200

    optimized

--- get_model.py
+++ get_model.py
@@ -1,29 +1,29 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.python import keras as k 
+from tensorflow.python.keras.optimizers import adam_v2 as o
 
-def get_model(X_train,lr,activation):
-    model = keras.Sequential()
-
-    # model.add(layers.Embedding(input_dim=1000, output_dim=4))
+def get_model(X_train,lr,activation,dropout):
+    model = k.Sequential()
     max_seq = X_train.bounding_shape()[-1]
     print(max_seq)
 
-    model.add(layers.Input(shape=[None, max_seq], dtype=tf.float32, ragged=True))  #Warning: use InputLayer?
+    model.add(k.layers.Input(shape=[None, max_seq], dtype=tf.float32, ragged=True))  #Warning: use InputLayer?
     #, batch_size=99
     
-    model.add(layers.LSTM(200)) #128
-    model.add(layers.Dropout(0.01)) #0.2
+    model.add(k.layers.LSTM(128)) #128
+    model.add(k.layers.Dropout(dropout)) #0.2
 
 
-    model.add(layers.Dense(64, activation=activation)) #32,relu
-    model.add(layers.Dropout(0.01)) #0.2
+    model.add(k.layers.Dense(64, activation=activation)) #64 #32,relu
+    model.add(k.layers.Dropout(dropout)) #0.2
 
-    model.add(layers.Dense(4, activation='softmax')) #class - 4
+    model.add(k.layers.Dense(4, activation='softmax')) #='softmax' #class - 4????
     # model.add(layers.Dropout(0.2))
 
-    opt = tf.keras.optimizers.Adam(lr=lr) #0.001, decay=1e-5
+    # opt = tf.keras.optimizers.Adam(lr=lr) #0.001, decay=1e-5
+    opt = o.Adam(lr=lr) #0.001, decay=1e-5
+
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
     model.summary()
 
--- time_series_clf.py
+++ time_series_clf.py
@@ -1,9 +1,13 @@
 from recurent_nn import *
+from get_model import *
+import tensorflow as tf 
 from tensorflow import keras
+from decouple import config
 # from tensorflow.keras import layers
 import time
 import numpy as np
 import pandas as pd
+import os
 from pandas.core.frame import DataFrame
 
 def find_min_t(X_test):
@@ -18,36 +22,55 @@ def find_min_t(X_test):
 def export_results2(score,acc):
     
     ExportToFile="rnn-test-time-series-clf"+time.strftime("%Y-%m-%d-%H-%M-%S")+".csv"
-    folder = 'D:\\handwritten-analysis\\results-test\\'
+    os.environ['EXPORT'] = ExportToFile
+    ExportToFile = os.environ.get('EXPORT')
+    # folder = 'D:\\handwritten-analysis\\results-test\\'
+    folder = config('FOLDER')
     
     prediction = pd.DataFrame(list(zip(score, acc)),
                columns =['scores', 'accuracy'])
     # prediction.index = X_test.index # its important for comparison
-    prediction.to_csv(folder+ExportToFile)
+    prediction.to_csv(os.path.join(folder+ExportToFile))
     print(prediction)
     return
 
 
-def real_time(model,X_train, Y_train, X_test, Y_test):
+def real_time(X_train, Y_train, X_test, Y_test,parameters):
     score_list = []
     accuracy_list = []
-    epochs = 1
+    epochs = parameters[1]
+    lr = parameters[2]  
+    activation = parameters[3]
+    dropout = parameters[4]
+
+    X_train = tf.ragged.constant(X_train)
+    Y_train = tf.ragged.constant(Y_train).to_tensor()
+    Y_test = tf.ragged.constant(Y_test).to_tensor()
+
+    
+    model = get_model(X_train,lr,activation,dropout)
+
+    X_train = X_train.to_tensor()
+
     
     print('----------real_time------------')
     min_t = find_min_t(X_test)
-    step =  min_t // 3
+    step =  min_t // 3 #only min_t
     print('min_t',min_t)
 
-    for i in range(1,min_t,step):
+    for i in range(1,min_t+1,step):
 
         new_test_X = X_test[:]
         for x in range(len(X_test)):
-            new_test_X[x] = new_test_X[x][:i]  ###IGNORE 
-        X_test1 = tf.ragged.constant(new_test_X)
-        X_test1 = X_test1.to_tensor()
+            new_test_X[x] = new_test_X[x][:i]  ###cut data 
+            if x==0:
+                print('x',new_test_X[x])
+
+        X_test1 = tf.ragged.constant(new_test_X).to_tensor()
+    
 
 
-        print(X_test1.shape)
+        print('-----X_test1.shape',X_test1.shape)
 
         model.fit(X_train, Y_train, 
                     epochs=epochs, batch_size=1,
